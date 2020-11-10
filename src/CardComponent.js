decbe014b64ba13cf4f370f5cbeb0573cd6c0301
Christopher Turton <christopheraturton@gmail.com>
2020-11-10 13:28:56 -0500

    Created json for Robots and finished psuedo dom layout

--- src/CardComponent.js
+++ src/CardComponent.js
@@ -1,19 +1,18 @@
 import React from 'react';
 import {
     Card, CardImg, CardText, CardBody,
-    CardTitle, CardSubtitle, Button
+    CardTitle
 } from 'reactstrap';
 import "./CardComponent.css"
 
-const CardComponent = () => {
+
+const CardComponent = ({ id, name, email}) => {
     return (
-        <Card className="cardCom hvr-grow">
-            <CardImg className="cardImg" src="https://robohash.org/a" alt="Card image cap" />
-            <CardBody>
-                <CardTitle tag="h5">Card title</CardTitle>
-                <CardSubtitle tag="h6" className="mb-2 text-muted">Card subtitle</CardSubtitle>
-                <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
-                <Button>Button</Button>
+        <Card className="cardCom hvr-grow shadow-lg bg-danger">
+            <CardImg className="cardImg" src={`https://robohash.org/${id}`} alt="Robot Image" />
+            <CardBody className="cardBody">
+                <CardTitle tag="h5">{name}</CardTitle>
+                <CardText>{email}</CardText>
             </CardBody>
         </Card>
     )
