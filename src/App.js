f078f58efb18f98f791a2b06d09e99b54535b38d
Christopher Turton <christopheraturton@gmail.com>
2020-11-17 13:26:22 -0500

    added an componentDidMount feature to make a call to a name  placeholder  API for the robots as well as a loading splash screen if there are robots to be loaded on the page

--- src/App.js
+++ src/App.js
@@ -1,34 +1,42 @@
 import React, { Component } from 'react'
 import Cardlist from './Cardlist'
-import { robots } from './creatures'
+// import { robots } from './creatures'
 import SearchBox from './SearchBox'
-import "./App.css" 
+import "./App.css"
 
 class App extends Component {
     constructor() {
         super()
         this.state = {
-            robots: robots,
+            robots: [],
             searchfield: ''
         }
     }
 
+    componentDidMount() {
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then(response => response.json())
+            .then(users => this.setState({ robots: users }))
+    }
     onSearchChange = (event) => {
-        this.setState ({ searchfield: event.target.value })
+        this.setState({ searchfield: event.target.value })
     }
 
     render() {
         const filteredRobots = this.state.robots.filter(robots => {
             return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase())
         })
-    
-    return (
-        <div className="tc">
-            <h1 className='f1'> ROBOfriends </h1>
-            <SearchBox searchChange={this.onSearchChange} />
-            <Cardlist robots={filteredRobots} />
-        </div>
-    )
+        if (this.state.robots.length === 0) {
+            return <h1>Loading</h1>
+        } else {
+            return (
+                <div className="tc">
+                    <h1 className='f1'> ROBOfriends </h1>
+                    <SearchBox searchChange={this.onSearchChange} />
+                    <Cardlist robots={filteredRobots} />
+                </div>
+            )
+        }
     }
 }
 
