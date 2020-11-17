fb96dfdfa628c3962ed9f824ffd196318a5c8140
Christopher Turton <christopheraturton@gmail.com>
2020-11-17 15:08:41 -0500

    added ErrorBoundry for Cardlist

--- src/containers/App.js
+++ src/containers/App.js
@@ -3,6 +3,7 @@ import Cardlist from '../components/Cardlist'
 import Scroll from '../components/Scroll'
 // import { robots } from './creatures'
 import SearchBox from '../components/SearchBox'
+import ErrorBoundry from '../components/ErrorBoundry'
 import "./App.css"
 
 class App extends Component {
@@ -24,23 +25,26 @@ class App extends Component {
     }
 
     render() {
-        const filteredRobots = this.state.robots.filter(robots => {
-            return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase())
+        const { robots, searchfield } = this.state;
+        const filteredRobots = robots.filter(robot => {
+            return robot.name.toLowerCase().includes(searchfield.toLowerCase())
         })
-        if (this.state.robots.length === 0) {
-            return <h1>Loading</h1>
-        } else {
-            return (
+       return !robots.length ?
+             <h1>Loading</h1>
+            :
+             (
                 <div className="tc">
                     <h1 className='f1'> ROBOfriends </h1>
                     <SearchBox searchChange={this.onSearchChange} />
                     <Scroll>
-                    <Cardlist robots={filteredRobots} />
+                        <ErrorBoundry>
+                            <Cardlist robots={filteredRobots} />
+                        </ErrorBoundry>
                     </Scroll>
                 </div>
             )
         }
     }
-}
+
 
 export default App;
